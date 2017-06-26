1d06e81191021d212098d43175ba529a2e067736
caojungang <caojungang@sogou-inc.com>
2017-06-26 11:23:15 +0800

    release v1.1.3

--- bin/build.js
+++ bin/build.js
@@ -1,16 +1,10 @@
-const { resolve } = require('path')
+const { join } = require('path')
 
 const { fork } = require('child_process')
 
-const gulpcli = resolve(
-  __dirname,
-  '..',
-  'node_modules',
-  'gulp-cli',
-  'bin',
-  'gulp'
-)
-const gulpfile = resolve(__dirname, '..', 'config', 'gulpfile.js')
+const gulpcli = join(require.resolve('gulp'), '../..', 'gulp', 'bin', 'gulp')
+
+const gulpfile = join(__dirname, '..', 'config', 'gulpfile.js')
 
 module.exports = function() {
   fork(gulpcli, ['--gulpfile', gulpfile, '--cwd', process.cwd(), '--color'], {
